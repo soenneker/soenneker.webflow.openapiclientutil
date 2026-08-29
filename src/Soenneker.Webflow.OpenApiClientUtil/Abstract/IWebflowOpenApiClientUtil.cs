b63eecdf5f13dfb03a03f6ecfda9737f8288dbc9
soenneker <jake@soenneker.com>
2026-08-29 14:17:27 -0500

    Improve public API XML documentation

--- src/Soenneker.Webflow.OpenApiClientUtil/Abstract/IWebflowOpenApiClientUtil.cs
+++ src/Soenneker.Webflow.OpenApiClientUtil/Abstract/IWebflowOpenApiClientUtil.cs
@@ -10,5 +10,10 @@ namespace Soenneker.Webflow.OpenApiClientUtil.Abstract;
 /// </summary>
 public interface IWebflowOpenApiClientUtil: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Returns the configured webflow OpenAPI Client used by the Webflow OpenAPI Client.
+    /// </summary>
+    /// <param name="cancellationToken">Token used to cancel the operation.</param>
+    /// <returns>A task whose result is the requested webflow OpenAPI Client.</returns>
     ValueTask<WebflowOpenApiClient> Get(CancellationToken cancellationToken = default);
 }
--- src/Soenneker.Webflow.OpenApiClientUtil/Registrars/WebflowOpenApiClientUtilRegistrar.cs
+++ src/Soenneker.Webflow.OpenApiClientUtil/Registrars/WebflowOpenApiClientUtilRegistrar.cs
@@ -13,6 +13,8 @@ public static class WebflowOpenApiClientUtilRegistrar
     /// <summary>
     /// Adds <see cref="WebflowOpenApiClientUtil"/> as a singleton service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddWebflowOpenApiClientUtilAsSingleton(this IServiceCollection services)
     {
         services.AddWebflowOpenApiHttpClientAsSingleton()
@@ -24,6 +26,8 @@ public static class WebflowOpenApiClientUtilRegistrar
     /// <summary>
     /// Adds <see cref="WebflowOpenApiClientUtil"/> as a scoped service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddWebflowOpenApiClientUtilAsScoped(this IServiceCollection services)
     {
         services.AddWebflowOpenApiHttpClientAsSingleton()
